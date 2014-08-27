cc4454174ce76e58b1ab27adfa2b942924d8f39d
shouichitamura <tamtam0303@gmail.com>
2014-08-27 23:04:28 +0900

    fix

--- src/net/nextscape/tamura/test/hello/ButtonActivity.java
+++ src/net/nextscape/tamura/test/hello/ButtonActivity.java
@@ -4,9 +4,7 @@
 package net.nextscape.tamura.test.hello;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.appcompat.R.string;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
